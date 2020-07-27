93d26bae5975f3db90aa20fd1fc266a3acaedb2d
Azucena Herrera <azucenaphx@gmail.com>
2020-07-27 14:41:15 -0700

    Updates

---
+++ precache-manifest.12110f59221c1a3bcdff40fc52a796ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e635c5b7bc67e809971c4c2b9c59c4",
+    "url": "/azucena/index.html"
+  },
+  {
+    "revision": "4c562b12c3bcfaccd1af",
+    "url": "/azucena/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab8f8261d0a7c3b4696f",
+    "url": "/azucena/static/js/2.edb5bf43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/azucena/static/js/2.edb5bf43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c562b12c3bcfaccd1af",
+    "url": "/azucena/static/js/main.6f324d69.chunk.js"
+  },
+  {
+    "revision": "5b92074ad066cd261301",
+    "url": "/azucena/static/js/runtime-main.d1616748.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/azucena/static/media/logo.5d5d9eef.svg"
+  }
+]);
